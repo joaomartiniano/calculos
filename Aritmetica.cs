5e1076b2a66b04bec0388171cb4dc84dff425ac9
João Martiniano <44736477+joaomartiniano@users.noreply.github.com>
2023-08-31 17:16:06 +0100

    Adicionar o método Multiplicar()

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -28,5 +28,14 @@ namespace Calculos
         {
             return x - y;
         }
+
+        /// <summary>
+        /// Operação de multiplicação.
+        /// </summary>
+        /// <returns>Retorna o resultado da multiplicação de dois números.</returns>
+        public static int Multiplicar(int x, int y)
+        {
+            return x * y;
+        }
     }
 }
