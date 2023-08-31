a97e0ade9e4f316b1335de72dee906c93cf7f25d
João Martiniano <44736477+joaomartiniano@users.noreply.github.com>
2023-08-31 16:58:58 +0100

    Acrescentar código para testar

--- Program.cs
+++ Program.cs
@@ -15,6 +15,11 @@ namespace Calculos
             Console.WriteLine($"4 + 2 = {Aritmetica.Somar(4, 2)}");
             Console.WriteLine($"4 - 2 = {Aritmetica.Subtrair(4, 2)}");
 
+            double t1 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
+            double t2 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsius, 100);
+
+            Console.WriteLine($"36º Celsius = {t1}º Fahrenheit");
+            Console.WriteLine($"100º Fahrenheit = {t2}º Celsius");
 
             Console.ReadKey();
         }
